353e2ea66b129ec7b47fe695fb1c8df10c8ea0e1
koketani <ykoketani@gmail.com>
2020-03-28 08:35:56 +0900

    fixup! move timing of creating tmux log directory

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ here=$(cd "$(dirname "$0")" && pwd -P)
 platform=$(uname -s)
 
 # under "$HOME"
-mkdir -p "$HOME"/.tmux/log
 ln -Ffs "$here"/tmux.conf "$HOME"/.tmux.conf
 ## For Linux, load the default profile
 [ "$platform" = "Darwin" ] && ln -Ffs "$here"/bash_profile "$HOME"/.bash_profile
@@ -16,9 +15,9 @@ ln -Ffs "$here"/agignore "$HOME"/.agignore
 ln -Ffs "$here"/tslint.json "$HOME"/.tslint.json
 ln -Ffs "$here"/eslintrc.json "$HOME"/.eslintrc.json
 ln -Ffs "$here"/vimrc "$HOME"/.vimrc
-## # under "$HOME"/.vim
-## mkdir -p "$HOME"/.vim
-## ln -Ffs $here/vim_config "$HOME"/.vim/_config
+
+# under "$HOME"/.tmux
+mkdir -p "$HOME"/.tmux/log
 
 # under "$HOME"/.config/pet
 mkdir -p "$HOME"/.config
