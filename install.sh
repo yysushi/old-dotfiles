4c385e0b4fb6e8e1e370ed4ce785c851384fb5ee
koketani <ykoketani@gmail.com>
2020-03-28 11:23:18 +0900

    fix how to install pet directory

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ mkdir -p "$HOME"/.tmux/log
 
 # under "$HOME"/.config/pet
 mkdir -p "$HOME"/.config
-ln -Ffs "$here"/pet "$HOME"/.config/pet
+[[ -d "$HOME"/.config/pet ]] || ln -Ffs "$here"/pet "$HOME"/.config/pet
 
 # under "$HOME"/Developments
 mkdir -p "$HOME"/Developments
