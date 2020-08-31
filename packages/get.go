750dba5581d829614883095ffdda827b4d74551b
koketani <ykoketani@gmail.com>
2020-08-31 19:45:27 +0900

    remove d option when go get

--- packages/get.go
+++ packages/get.go
@@ -1,20 +1,20 @@
 package packages
 
 // vim-go https://github.com/fatih/vim-go/blob/a3a9fbbd2436e840103f1addfe1534224ee5c4ab/plugin/go.vim#L43
-//go:generate go get -d github.com/klauspost/asmfmt/cmd/asmfmt
-//go:generate go get -d github.com/go-delve/delve/cmd/dlv
-//go:generate go get -d github.com/kisielk/errcheck
-//go:generate go get -d github.com/davidrjenni/reftools/cmd/fillstruct
-//go:generate go get -d github.com/rogpeppe/godef
-//go:generate go get -d golang.org/x/tools/cmd/goimports
-//go:generate go get -d golang.org/x/lint/golint
-//go:generate go get -d golang.org/x/tools/gopls
-//go:generate go get -d github.com/golangci/golangci-lint/cmd/golangci-lint
-//go:generate go get -d github.com/fatih/gomodifytags
-//go:generate go get -d golang.org/x/tools/cmd/gorename
-//go:generate go get -d github.com/jstemmer/gotags
-//go:generate go get -d golang.org/x/tools/cmd/guru
-//go:generate go get -d github.com/josharian/impl
-//go:generate go get -d honnef.co/go/tools/cmd/keyify
-//go:generate go get -d github.com/fatih/motion
-//go:generate go get -d github.com/koron/iferr
+//go:generate go get github.com/klauspost/asmfmt/cmd/asmfmt
+//go:generate go get github.com/go-delve/delve/cmd/dlv
+//go:generate go get github.com/kisielk/errcheck
+//go:generate go get github.com/davidrjenni/reftools/cmd/fillstruct
+//go:generate go get github.com/rogpeppe/godef
+//go:generate go get golang.org/x/tools/cmd/goimports
+//go:generate go get golang.org/x/lint/golint
+//go:generate go get golang.org/x/tools/gopls
+//go:generate go get github.com/golangci/golangci-lint/cmd/golangci-lint
+//go:generate go get github.com/fatih/gomodifytags
+//go:generate go get golang.org/x/tools/cmd/gorename
+//go:generate go get github.com/jstemmer/gotags
+//go:generate go get golang.org/x/tools/cmd/guru
+//go:generate go get github.com/josharian/impl
+//go:generate go get honnef.co/go/tools/cmd/keyify
+//go:generate go get github.com/fatih/motion
+//go:generate go get github.com/koron/iferr
