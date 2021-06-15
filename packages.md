b522e8ba98e997169c49f43adc35bd48ec179a84
koketani <ykoketani@gmail.com>
2021-06-15 09:30:53 +0900

    update golang version and how to install golang tools

--- packages.md
+++ packages.md
@@ -1,6 +1,6 @@
 # Language-Specific Tools' Installation
 
-- Golang: `go generate packages/get.go`
+- Golang: `go generate packages/install.go`
 - Python (pip): `PIP_REQUIRE_VIRTUALENV="" pip install -r packages/pip.txt`
 - Python (pipx): `<packages/pipx.txt xargs -I % pipx install %`
 - Rust (cargo): `<packages/cargo.txt xargs -J % cargo install %`
---
+++ packages/install.go
@@ -0,0 +1,29 @@
+package packages
+
+// vim-go https://github.com/fatih/vim-go/blob/a3a9fbbd2436e840103f1addfe1534224ee5c4ab/plugin/go.vim#L43
+//go:generate go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
+//go:generate go install github.com/go-delve/delve/cmd/dlv@latest
+//go:generate go install github.com/kisielk/errcheck@latest
+//go:generate go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
+//go:generate go install github.com/rogpeppe/godef@latest
+//go:generate go install golang.org/x/tools/cmd/goimports@latest
+//go:generate go install golang.org/x/lint/golint@latest
+//go:generate go install golang.org/x/tools/gopls@latest
+////go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+//go:generate go install github.com/fatih/gomodifytags@latest
+//go:generate go install golang.org/x/tools/cmd/gorename@latest
+//go:generate go install github.com/jstemmer/gotags@latest
+//go:generate go install golang.org/x/tools/cmd/guru@latest
+//go:generate go install github.com/josharian/impl@latest
+//go:generate go install honnef.co/go/tools/cmd/keyify@latest
+//go:generate go install github.com/fatih/motion@latest
+//go:generate go install github.com/koron/iferr@latest
+
+//// pet
+////go:generate go install github.com/knqyf263/pet@latest
+
+// godoc
+//go:generate go install golang.org/x/tools/cmd/godoc@latest
+
+// go-callvis https://github.com/ofabry/go-callvis#installation
+//go:generate go install github.com/ofabry/go-callvis@latest
