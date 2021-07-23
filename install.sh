a0efc10e3ea6a86eacc76d9e5338d1e2fcd43fc7
koketani <ykoketani@gmail.com>
2021-07-24 07:56:42 +0900

    add flake8 configuration

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ ln -Ffs "$here"/vimrc "$HOME"/.vimrc
 ln -Ffs "$here"/shellcheckrc "$HOME"/.shellcheckrc
 ln -Ffs "$here"/tool-versions "$HOME"/.tool-versions
 ln -Ffs "$here"/clippy.toml "$HOME"/.clippy.toml
+ln -Ffs "$here"/flake8 "$HOME"/.flake8
 ## for darwin
 [[ "$platform" == Darwin  ]] && ln -Ffs "$here"/bash_profile "$HOME"/.bash_profile
 
