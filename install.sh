80593f6032fb796911ae031a02ff982cbd03f549
hanaugai <ykoketani@gmail.com>
2022-01-09 15:04:13 -0800

    remove gitui

--- install.sh
+++ install.sh
@@ -26,7 +26,6 @@ mkdir -p "$HOME"/.tmux/log
 mkdir -p "$HOME"/.config
 # under "$HOME"/.config
 ln -Ffs "$here"/cargo-atcoder.toml "$HOME"/.config
-ln -Ffs "$here"/gitui "$HOME"/.config
 ln -Ffs "$here"/pet "$HOME"/.config
 ln -Ffs "$here"/yamllint "$HOME"/.config
 [[ -d "$HOME"/.config/nvim ]] || ln -Ffs "$here"/nvim "$HOME"/.config
