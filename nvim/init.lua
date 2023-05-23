09453b497246cb06b713f6c9c5a157ae74221a39
yysushi <yoshihiro.tsuji2@gmail.com>
2023-05-23 16:47:20 +0900

    add mapping config for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
 require('config.opt')
+require('config.mapping')
 
 require('plugins')
---
+++ nvim/lua/config/mapping.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', '<Leader>q', [[ <Cmd>:q<CR> ]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>Q', [[ <Cmd>:qa!<CR> ]], {noremap = true})
