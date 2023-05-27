45eff7fa4f7f7cee93658771d7b1d1defcb3f87f
yysushi <yoshihiro.tsuji2@gmail.com>
2023-05-27 00:44:12 +0000

    pin home-manager version in nixos-wsl

--- nixos/customize.nix
+++ nixos/customize.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
 in
 {
   imports = [
