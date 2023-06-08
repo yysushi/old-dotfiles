4ac79625ac2de981d50b3c30796bcad1081f735e
yysushi <yoshihiro.tsuji2@gmail.com>
2023-06-09 06:43:56 +0900

    udpate telescope to show hidden

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -18,7 +18,8 @@ telescope.setup({
   -- },
   pickers = {
     find_files = {
-      disable_devicons = true
+      hidden = true,
+      disable_devicons = true,
     },
     live_grep = {
       disable_devicons = true
