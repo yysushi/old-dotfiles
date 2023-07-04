be869a2593bffed373fb4ce934c9b9ecf3bf4e20
yysushi <yoshihiro.tsuji2@gmail.com>
2023-07-04 09:42:41 +0900

    add mapping for ale fix

--- nvim/lua/config/mapping.lua
+++ nvim/lua/config/mapping.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap('n', '<Leader>q', [[ <Cmd>:q<CR> ]], {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>Q', [[ <Cmd>:qa!<CR> ]], {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>c', [[ <Cmd>:cclose<bar>lclose<CR> ]], {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>f', [[ <Cmd>:ALEFix<CR> ]], {noremap = true})
