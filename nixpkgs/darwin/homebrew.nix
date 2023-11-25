2668e69db8f82c3b3923dff735ba73f5fd36f48d
yysushi <yoshihiro.tsuji2@gmail.com>
2023-11-26 07:29:34 +0900

    add neardrop

--- nixpkgs/darwin/homebrew.nix
+++ nixpkgs/darwin/homebrew.nix
@@ -11,7 +11,13 @@
       autoUpdate = true;
       brewfile = true;
     };
+    taps = [
+      # https://github.com/grishka/NearDrop
+      "grishka/grishka"
+    ];
     brews = [
+      # "grishka/grishka/neardrop"
+
       "openssl@1.1"
       "socket_vmnet"
     ];
