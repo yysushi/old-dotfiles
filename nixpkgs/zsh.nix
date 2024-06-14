3e61650035985d0ef77fb9eb0999ec657fbb35a2
yysushi <yoshihiro.tsuji2@gmail.com>
2024-06-14 10:58:47 +0900

    update order of commands in nixpkgs/zsh.nix

--- nixpkgs/zsh.nix
+++ nixpkgs/zsh.nix
@@ -2,21 +2,23 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = false;
     shellAliases = {
       cat = "bat";
     };
     autocd = true;
     profileExtra = ''
       DISPLAY="$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0"
-      bindkey -v
       setopt no_beep
       # TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
       # command -v limactl > /dev/null && TESTCONTAINERS_HOST_OVERRIDE=$(limactl shell default ip a show lima0 | grep 'inet '| awk '{print $2}' | cut -f1 -d"/")
     '';
     initExtra = ''
+      bindkey -v
+      bindkey '^N' autosuggest-accept
+
       eval "$(sheldon source)"
 
-      bindkey '^N' autosuggest-accept
       . "${config.home.homeDirectory}/.dotfiles/agkozak_prompt.zsh"
     '';
   };
