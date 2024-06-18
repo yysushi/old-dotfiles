0516dc664f69c2e226ae528acb8476dddd4bc67c
yysushi <yoshihiro.tsuji2@gmail.com>
2024-06-18 11:34:06 +0000

    rebase nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,28 +1,30 @@
-{ pkgs, config, modulesPath, lib, ... }:
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-let
-  nixos-wsl = import /etc/nixos/nixos-wsl;
-in
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
 {
   imports = [
-    nixos-wsl.nixosModules.wsl
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+
+    # customize
     /home/nixos/.dotfiles/nixos/customize.nix
   ];
 
-  wsl = {
-    enable = true;
-    wslConf.automount.root = "/mnt";
-    defaultUser = "nixos";
-    startMenuLaunchers = true;
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
 
-    docker-native.enable = false;
-    # docker-desktop.enable = true;
-  };
 
-  nix.package = pkgs.nixFlakes;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
-  system.stateVersion = "22.11";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
