5de3ac83a58607d50c97c40a36d7e52d1c5f3a3e
yysushi <yoshihiro.tsuji2@gmail.com>
2024-12-14 07:41:16 -0800

    jiro: add fprintd

--- jiro/configuration.nix
+++ jiro/configuration.nix
@@ -112,7 +112,15 @@
      git
 
      chromium
+
+     usbutils
+
+     fprintd
   ];
+
+  # to check device, execute `lsusb | grep -i finger`
+  # to enroll, execute `fprintd-enroll`
+  services.fprintd.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
