166d82faadcecde35151265063d578dcf38293f2
yysushi <yoshihiro.tsuji2@gmail.com>
2024-12-28 02:30:09 -0800

    jiro: fixing

--- jiro/home/stylix.nix
+++ jiro/home/stylix.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
-  # stylix = {
-  # };
+  stylix = {
+    enable = true;
+    image = ./nix-wallpaper-simple-red.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+  };
 }
